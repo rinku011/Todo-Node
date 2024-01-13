e5a33b5451fac4a0ae5a48c951e3103891b8218a
Rinku <rkd302012@gmail.com>
2024-01-13 23:49:13 +0530

    fixed cors again

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ config({
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin : [process.env.FRONTEND_URL],
+  origin : process.env.FRONTEND_URL,
   methods : ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: "Content-Type",
   credentials : true,
