63a80297d5b198586ac1566b6d9b8215c5f51ed7
Rinku <rkd302012@gmail.com>
2024-01-17 22:22:51 +0530

    cors updated

--- app.js
+++ app.js
@@ -18,7 +18,12 @@ config({
 // middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors())
+app.use(cors({
+  origin: 'https://todo-client-vqer.onrender.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: 'Content-Type',
+  credentials: true,
+}));
 
 //using routes
 app.use("/api/v1/users", userRouter);
