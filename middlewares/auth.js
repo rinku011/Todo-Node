658cbd5fbaf25c730661c79a09645da42e7eeeb2
Rinku <rkd302012@gmail.com>
2024-01-18 16:14:33 +0530

    updated header thing

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,14 @@ import jwt from "jsonwebtoken"
 
 export const isAuthenticated = async(req,res,next)=>{
   
-  const token = req.cookies.token;
+  const authorizationHeader = req.headers.authorization;
+  if (!authorizationHeader) {
+    return res.status(401).json({
+      success: false,
+      message: "Authorization header missing. Login first.",
+    });
+  }
+  const token = authorizationHeader.split(" ")[1];
   
     if(!token)
       return res.status(404).json({
